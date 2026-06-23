0f9b22c5a3a7173ee4f3d27fdcf0a5ed491d5b7f
Roghit <roghitv2003@gmail.com>
2026-06-23 16:32:31 +0530

    Spring Security

---
+++ src/main/java/com/LaunchPad/login/AppSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.LaunchPad.login;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig{
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        List<UserDetails> users= new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("roghit").password("1234").roles("USER").build());
+        return new InMemoryUserDetailsManager(users);
+    }
+}
