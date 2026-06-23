0d20e56d63fb7e2bea7a5458300ab3dd34b4d406
Roghit <roghitv2003@gmail.com>
2026-06-23 21:01:04 +0530

    Login & Postgres connection

--- src/main/java/com/LaunchPad/login/AppSecurityConfig.java
+++ src/main/java/com/LaunchPad/login/AppSecurityConfig.java
@@ -1,23 +1,34 @@
 package com.LaunchPad.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig{
+    @Autowired
+    private MyUserDetailService userDetailsService;
+
     @Bean
-    protected UserDetailsService userDetailsService(){
-        List<UserDetails> users= new ArrayList<>();
-        users.add(User.withDefaultPasswordEncoder().username("roghit").password("1234").roles("USER").build());
-        return new InMemoryUserDetailsManager(users);
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider provider= new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
     }
+
+
+//    @Bean
+//    protected UserDetailsService userDetailsService(){
+//        List<UserDetails> users= new ArrayList<>();
+//        users.add(User.withDefaultPasswordEncoder().username("roghit").password("1234").roles("USER").build());
+//        return new InMemoryUserDetailsManager(users);
+//    }
 }
